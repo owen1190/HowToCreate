eaaed84239e9158f29f2838d2a2bd93c3d511745
owen-yu <owen1190@sina.cn>
2017-06-03 10:28:32 +0800

    format document

--- Calendar/index.js
+++ Calendar/index.js
@@ -1,27 +1,31 @@
-
 var nyear = new Date().getFullYear();
-var nmonth = new Date().getMonth()+1;
-var monthName=["","January" ,"February","March","April","May" ,
-"June" ,"July","August" ,"September" ,"October" ,"November","December"]
-calendar(nyear,nmonth);
-showDate(nyear,monthName[nmonth])
+var nmonth = new Date().getMonth() + 1;
+var monthName = ["", "January", "February", "March", "April", "May",
+    "June", "July", "August", "September", "October", "November", "December"
+]
+calendar(nyear, nmonth);
+showDate(nyear, monthName[nmonth])
 
 var prev = document.getElementsByClassName('prev')[0];
 var next = document.getElementsByClassName('next')[0];
-prev.addEventListener('click',function(){
+prev.addEventListener('click', function () {
     nmonth--;
-    if(nmonth<1) {nmonth=12,nyear--}
-    calendar(nyear,nmonth);
-    showDate(nyear,monthName[nmonth])
-},false)
-next.addEventListener('click',function(){
+    if (nmonth < 1) {
+        nmonth = 12, nyear--
+    }
+    calendar(nyear, nmonth);
+    showDate(nyear, monthName[nmonth])
+}, false)
+next.addEventListener('click', function () {
     nmonth++;
-    if(nmonth>12) {nmonth=1,nyear++}
-    calendar(nyear,nmonth);
-    showDate(nyear,monthName[nmonth])
-},false)
+    if (nmonth > 12) {
+        nmonth = 1, nyear++
+    }
+    calendar(nyear, nmonth);
+    showDate(nyear, monthName[nmonth])
+}, false)
 
-function showDate(nyear,nmonth){
+function showDate(nyear, nmonth) {
     var year = document.getElementById('year');
     var month = document.getElementById('month');
     year.innerText = nyear;
@@ -29,14 +33,14 @@ function showDate(nyear,nmonth){
 }
 
 function calendar(year, month) {
-    var bLeap = (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;//判断闰年否
-    var nCount = [0, 31, bLeap ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month] || 0;//当月天数
-    var nStart = new Date(year, month - 1, 1).getDay();//开始星期几
+    var bLeap = (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0; //判断闰年否
+    var nCount = [0, 31, bLeap ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month] || 0; //当月天数
+    var nStart = new Date(year, month - 1, 1).getDay(); //开始星期几
     nStart = (nStart - 1 + 7) % 7;
     // 当前
     var oNow = new Date();
-    var nNowDate = oNow.getDate();//当前几号
-    var bCurrent = oNow.getFullYear() === year && oNow.getMonth() + 1 === month;//是否为今年或今月
+    var nNowDate = oNow.getDate(); //当前几号
+    var bCurrent = oNow.getFullYear() === year && oNow.getMonth() + 1 === month; //是否为今年或今月
 
     // 渲染
     var aTd = [].slice.call(document.getElementsByTagName('td'), 0);
--- Slideshow/index.js
+++ Slideshow/index.js
@@ -9,21 +9,25 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-function showSlides(n) {  
+function showSlides(n) {
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
   // 最后一张点击右箭头，则返回到第一张
-  if (n > slides.length) {slideIndex = 1} 
+  if (n > slides.length) {
+    slideIndex = 1
+  }
   // 第一张点击左箭头，则返回最后一张
-  if (n < 1) {slideIndex = slides.length}
+  if (n < 1) {
+    slideIndex = slides.length
+  }
   // 隐藏所有照片
   for (var i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
+    slides[i].style.display = "none";
   }
   for (var i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
   // 显示照片，并改变点的状态
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
