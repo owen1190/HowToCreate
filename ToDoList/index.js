1e26e928e22e215d15d2174aa5a38ed9a4b782d5
owen-yu <owen1190@sina.cn>
2017-06-08 09:47:28 +0800

    add ToDoList

---
+++ ToDoList/index.js
@@ -0,0 +1,56 @@
+var myNodelist = document.getElementsByTagName("LI");
+
+// 给每一行创建关闭符号
+for (var i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// 点击关闭按钮，则隐藏
+var close = document.getElementsByClassName("close");
+var i;
+for (var i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    // 获取到其父值li
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// 事件委托，点击每一行时，切换类名
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
